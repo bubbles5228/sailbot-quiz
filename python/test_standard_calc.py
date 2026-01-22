7c6090344c293445483393f5f88aa203d3e38325
bubbles5228 <lue.chen.chn@gmail.com>
2026-01-22 14:00:52 -0800

    Add additional tests for angle bounding functions
    not sure if this is the proper way to do it

--- python/test_standard_calc.py
+++ python/test_standard_calc.py
@@ -6,6 +6,12 @@ from standard_calc import bound_to_180, is_angle_between
 
 def test_bound_basic1():
     assert bound_to_180(0) == 0
+    assert bound_to_180(180) == 0
+    assert bound_to_180(-180) == 0
+    assert bound_to_180(6767) == 0
+    assert bound_to_180(-6767) == 0
+    assert bound_to_180(90) == 0
+    assert bound_to_180(-90) == 0
 
 
 """ Tests for is_angle_between() """
@@ -13,3 +19,9 @@ def test_bound_basic1():
 
 def test_between_basic1():
     assert is_angle_between(0, 1, 2)
+    assert is_angle_between(0, 50, 190)
+    assert is_angle_between(0, -10, 190)
+    assert is_angle_between(360, 365, 370)
+    assert is_angle_between(-90, 1, -89)
+    assert is_angle_between(203, 64, -3)
+    assert is_angle_between(67, 6767, -67)
